367be26c60dbb81c83a08ad19718b914cfe3f451
PT6402 <tranphat701@gmail.com>
2023-10-20 09:01:43 +0700

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace deploy_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
